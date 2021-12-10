e213cfc00c63f1dc2c140db6044f94f18d5cdf94
Pinponsuhu <pinponsuhuomorinsola@gmail.com>
2021-12-10 18:28:24 +0100

    hero

--- resources/views/layouts/hospital/app.blade.php
+++ resources/views/layouts/hospital/app.blade.php
@@ -12,11 +12,11 @@
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Cabin:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
 
-        <link rel="stylesheet" href="{{ asset_secure('css/app.css') }}">
-        <link rel="stylesheet" href="{{ asset_secure('css/jquery.dataTables.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/jquery.dataTables.css') }}">
 
-        <script src="{{ asset_secure('js/all.js') }}"></script>
-        <script src="{{ asset_secure('js/jquery.js') }}"></script>
+        <script src="{{ asset('js/all.js') }}"></script>
+        <script src="{{ asset('js/jquery.js') }}"></script>
 
         <!-- Styles -->
 
