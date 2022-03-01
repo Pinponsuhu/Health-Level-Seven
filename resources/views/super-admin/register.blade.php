60394218e1e8d417d34b526d43def938c1494d48
Pinponsuhu Joseph <pinponsuhuomorinsola@gmail.com>
2022-03-01 12:50:05 -0800

    Hospital ID

--- resources/views/super-admin/register.blade.php
+++ resources/views/super-admin/register.blade.php
@@ -52,42 +52,42 @@
             <h1 class="md:col-span-2 text-2xl font-semibold text-green-600 mb-3">Register new Hospital</h1>
             <div class="my-2">
                 <label class="font-semibold text-md block mb-1">Hospital Logo</label>
-                <input type="file" value="{{ old('hospital_logo') }}" name="hospital_logo" placeholder="Enter Patient's Surname" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
+                <input type="file" value="{{ old('hospital_logo') }}" name="hospital_logo" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
                 @error('hospital_logo')
                     <p class="text-sm text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div class="my-2">
                 <label class="font-semibold text-md block mb-1">Hospital Name</label>
-                <input type="text" value="{{ old('hospital_name') }}" name="hospital_name" placeholder="Enter Patient's Surname" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
+                <input type="text" value="{{ old('hospital_name') }}" name="hospital_name" placeholder="Enter Hospital Name" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
                 @error('hospital_name')
                     <p class="text-sm text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div class="my-2">
                 <label class="font-semibold text-md block mb-1">Hospital CEO</label>
-                <input type="text" value="{{ old('head_of_hospital') }}" name="head_of_hospital" placeholder="Enter Patient's Surname" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
+                <input type="text" value="{{ old('head_of_hospital') }}" name="head_of_hospital" placeholder="Enter Hospital " class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
                 @error('head_of_hospital')
                     <p class="text-sm text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div class="my-2">
                 <label class="font-semibold text-md block mb-1">Hospital Email Address</label>
-                <input type="text" value="{{ old('email_address') }}" name="email_address" placeholder="Enter Patient's Surname" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
+                <input type="text" value="{{ old('email_address') }}" name="email_address" placeholder="Enter Hospital Email" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
                 @error('email_address')
                     <p class="text-sm text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div class="my-2">
                 <label class="font-semibold text-md block mb-1">Hospital Phone Number</label>
-                <input type="text" value="{{ old('phone_number') }}" name="phone_number" placeholder="Enter Patient's Surname" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
+                <input type="text" value="{{ old('phone_number') }}" name="phone_number" placeholder="Enter Hospital Number" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
                 @error('phone_number')
                     <p class="text-sm text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div class="my-2">
                 <label class="font-semibold text-md block mb-1">Hospital Address</label>
-                <input type="text" value="{{ old('hospital_address') }}" name="hospital_address" placeholder="Enter Patient's Surname" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
+                <input type="text" value="{{ old('hospital_address') }}" name="hospital_address" placeholder="Enter Hospital Address" class="capitalize border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
                 @error('hospital_address')
                     <p class="text-sm text-red-500">{{ $message }}</p>
                 @enderror
@@ -108,14 +108,14 @@
             </div>
             <div class="my-2">
                 <label class="font-semibold text-md block mb-1">Password</label>
-                <input type="text" value="{{ old('password') }}" name="password" placeholder="Enter Patient's Surname" class="border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
+                <input type="text" value="{{ old('password') }}" name="password" placeholder="Enter Hospital Password" class="border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
                 @error('password')
                     <p class="text-sm text-red-500">{{ $message }}</p>
                 @enderror
             </div>
             <div class="my-2">
                 <label class="font-semibold text-md block mb-1">Confirm Password</label>
-                <input type="text" value="{{ old('confirm_password') }}" name="password_confirmation" placeholder="Enter Patient's Surname" class="border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
+                <input type="text" value="{{ old('confirm_password') }}" name="password_confirmation" placeholder="Confirm Hospital Password" class="border-l-4 border-green-500 w-full rounded-md p-3 shadow-md outline-none block">
                 @error('confirm_password')
                     <p class="text-sm text-red-500">{{ $message }}</p>
                 @enderror
