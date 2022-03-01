37c777ad7a80550bd25a5902962e6f9c909d69c7
Pinponsuhu Joseph <pinponsuhuomorinsola@gmail.com>
2022-03-01 13:38:22 -0800

    Hospital ID

--- resources/views/hospital/all-patient.blade.php
+++ resources/views/hospital/all-patient.blade.php
@@ -9,9 +9,7 @@
             <div class="p-2 md:p-4 bg-white py-4 mt-4 rounded-md shadow-md">
                 <form action="/view/all/patient" class="w-11/12 md:w-8/12 mx-auto grid capitalize grid-cols-4 gap-x-3 items-center my-3" method="get">
                     @csrf
-                    <input type="search" id="search" value="@isset($search)
-                    {{ $search }}
-                    @endisset" name="search" placeholder="Search By Surname, Patient ID, Email or Phone Number" class="bg-green-500 col-span-3 outline-none rounded-md shadow-md px-3 h-12 py-3 text-white placeholder-green-50 block">
+                    <input type="search" id="search" value="@isset($search){{ $search }}@endisset" name="search" placeholder="Search By Surname, Patient ID, Email or Phone Number" class="bg-green-500 col-span-3 outline-none rounded-md shadow-md px-3 h-12 py-3 text-white placeholder-green-50 block">
                     <button type="submit" class="w-full rounded-md shadow-md bg-green-500 block h-12 text-white">Search</button>
                 </form>
                 @isset ($search)
